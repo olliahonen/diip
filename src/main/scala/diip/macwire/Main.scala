1b668e88708ac9839b6a86e12fad9244a43f7e70
Olli Ahonen <olliahonen>
2018-03-15 13:00:37 +0200

    Add test registry

--- src/main/scala/diip/macwire/Main.scala
+++ src/main/scala/diip/macwire/Main.scala
@@ -4,13 +4,22 @@ import com.softwaremill.macwire.wire
 
 object Main {
   val words = Registry.wordService
+  val testWords = TestRegistry.wordService
 
   def run(): Unit =
     println(words.enlighten(4))
+
+  def test(): Unit =
+    println(testWords.enlighten(4))
 }
 
 object Registry {
   lazy val wordRepo = wire[WordRepo]
+  lazy val wordService = wire[WordService]
+}
+
+object TestRegistry {
+  lazy val wordRepo = wire[WordRepoPrint]
   lazy val wordService = wire[WordService]
 }
 
@@ -23,4 +32,11 @@ class WordService(wordRepo: WordRepo) {
 
 class WordRepo {
   def getWord = if (Math.random < 0.5) "yo" else "hej"
+}
+
+class WordRepoPrint extends WordRepo {
+  override def getWord = {
+    println("testing...")
+    "???"
+  }
 }
